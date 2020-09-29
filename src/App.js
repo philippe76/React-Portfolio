9f4ddbc572077e8fba9dd2ce5410f3ae723a7539
Philippe_Lanougadere <p.lanougadere@gmail.com>
2020-09-29 23:17:34 +0200

    start burger-menu / improve CV section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-// import Nav from "./components/Nav";
+import Nav from "./components/Nav";
 import Profile from "./components/Profile";
 import Project from "./components/Project";
 import Cv from "./components/Cv";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [bigWidth, setbigWidth] = useState(true);
+
+  // window.addEventListener("resize", () => {
+  //   setbigWidth(!bigWidth);
+  // });
+
+  useEffect(() => {
+    window.matchMedia("(max-width: 1220px)").matches && setbigWidth(false);
+  }, [bigWidth]);
+
   return (
     <div id="home">
-      <Navbar />
+      {bigWidth ? <Navbar /> : <Nav />}
       <Profile />
       <Project />
       <Cv />
--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,45 +1,72 @@
 import React from "react";
 // import { Document, Page, pdfjs } from "react-pdf";
 import CV from "../images/CV.pdf";
-import cvPic from "../images/cvPic.PNG";
+import cvPic from "../images/cvPic2.PNG";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faDownload,
   faExternalLinkAlt,
+  faGraduationCap,
+  faTruckMoving,
+  faBoxOpen,
+  faLaptopCode,
+  faEdit,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Cv = () => {
   // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
   return (
-    <div className="box" id="cv">
-      <h2 className="big-title"> {"[ Mon CV ]"} </h2>
-      <div className="cv-container">
-        <p className="paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Exercitationem iure commodi cupiditate, repellat dolorum quae soluta
-          aperiam quia distinctio beatae debitis, non labore tempore? Illo eum
-          numquam sapiente praesentium hic, vel rem ipsa, perferendis adipisci
-          eligendi similique, libero assumenda laudantium ullam! Accusamus unde
-          veritatis fugiat sint, a eius quasi magni enim molestiae reiciendis
-          maxime. Nisi ex dolorem esse voluptates unde necessitatibus at!
-          Laboriosam quisquam adipisci deleniti facilis similique sed temporibus
-          <br />
-          <a href={CV} download="Lanougadere_Philippe_CV">
+    <>
+      <div className="box" id="cv">
+        <h2 className="big-title"> {"[ Mon parcours ]"} </h2>
+        <div className="cv-container">
+          <FontAwesomeIcon icon={faGraduationCap} className="date" />
+          <FontAwesomeIcon icon={faBoxOpen} className="date" />
+          <FontAwesomeIcon icon={faEdit} className="date" />
+          <FontAwesomeIcon icon={faTruckMoving} className="date" />
+          <FontAwesomeIcon icon={faLaptopCode} className="date" />
+        </div>
+        <div className="cv-container">
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+        </div>
+        <div className="cv-container">
+          <p className="paragraph">Bac L</p>
+          <p className="paragraph">Manutention</p>
+          <p className="paragraph">Management</p>
+          <p className="paragraph">Poids-Lourd</p>
+          <p className="paragraph">Programmation</p>
+        </div>
+      </div>
+      <div className="box">
+        <h2 className="big-title"> {"[ Mon CV ]"} </h2>
+        <div className="cv-pic">
+          <p className="paragraph">
+            On dit qu'aujourd'hui une personne passe par deux reconversions
+            professionnelles la dernière s'est terminée sur une blessure Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Quasi,
+            blanditiis? Fuga inventore atque dignissimos recusandae accusamus
+            doloribus harum, hic pariatur sunt, eius sint in magni nostrum, quos
+            velit facere iure?
+            {/* <a href={CV} download="Lanougadere_Philippe_CV">
             <FontAwesomeIcon icon={faDownload} />
           </a>
           <a href={CV}>
             <FontAwesomeIcon icon={faExternalLinkAlt} />
-          </a>
-        </p>
+          </a> */}
+          </p>
 
-        <img src={cvPic} alt="" />
-
+          <img src={cvPic} alt="" />
+        </div>
         {/* <Document file={CV}>
           <Page pageNumber={1} />
         </Document> */}
       </div>
-    </div>
+    </>
   );
 };
 
