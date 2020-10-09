689228810727e9073c11cc19ed6035b040e16eae
Philippe_Lanougadere <p.lanougadere@gmail.com>
2020-10-09 15:10:25 +0200

    correct little typo in Profile section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -11,8 +11,8 @@ const Profile = () => {
           <img src={profile} alt="profile pic" />
           <p className="paragraph">
             Développeur Web JavaScript. En reconversion professionnelle après un
-            parcours de vie assez varié. Passé par management et la conduite de
-            poids lourds. Je prends aujourd'hui énormément de plaisir dans la
+            parcours de vie assez varié. Passé par le management et la conduite
+            de poids lourds. Je prends aujourd'hui énormément de plaisir dans la
             programmation. J'ai choisi de me spécialiser dans JavaScript et son
             environnement, du front-end avec React Js au back-end avec Node Js.
           </p>
