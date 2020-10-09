c00607d87b0bd51c6af4c180827aa072ea101d30
Philippe_Lanougadere <p.lanougadere@gmail.com>
2020-10-09 09:47:35 +0200

    update last details for responsive behavior

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,12 +7,12 @@ const Profile = () => {
     <>
       <section className="profile-box" id="profile">
         <div className="profile-card">
-          <h2 className="profile-title">- Philippe LANOUGADERE -</h2>
+          <h2 className="profile-title">Philippe LANOUGADERE</h2>
           <img src={profile} alt="profile pic" />
           <p className="paragraph">
             Développeur Web JavaScript. En reconversion professionnelle après un
-            parcours de vie assez varié. Passé du management à la conduite de
-            poids lourds. Je prends aujourd'hui énormément de plaisir avec la
+            parcours de vie assez varié. Passé par management et la conduite de
+            poids lourds. Je prends aujourd'hui énormément de plaisir dans la
             programmation. J'ai choisi de me spécialiser dans JavaScript et son
             environnement, du front-end avec React Js au back-end avec Node Js.
           </p>
