7d5323ff30f00601c2cc82b624a4d5baff860b86
Philippe <p.lanougadere@htomail.fr>
2024-01-13 19:15:14 +0100

    change CV file and preview

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,12 +9,13 @@ const Profile = () => {
         <div className="profile-card">
           <h2 className="profile-title">Philippe LANOUGADERE</h2>
           <img src={profile} alt="profile pic" />
-          <p className="paragraph">
-            Développeur Web JavaScript. En reconversion professionnelle après un
+          <p className="bio">
+            {/* Développeur Web JavaScript. En reconversion professionnelle après un
             parcours de vie assez varié. Passé par le management et la conduite
             de poids lourds. Je prends aujourd'hui énormément de plaisir dans la
             programmation. J'ai choisi de me spécialiser dans JavaScript et son
-            environnement, du front-end avec React Js au back-end avec Node Js.
+            environnement, du front-end avec React Js au back-end avec Node Js. */}
+            Autodidacte de formation, je suis formateur en développement web depuis maintenant 3 ans
           </p>
         </div>
         <Stack />
