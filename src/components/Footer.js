6c8b452d0cbbea138bc9a7d9c3147177dc3112d8
Philippe <p.lanougadere@htomail.fr>
2024-01-13 08:57:46 +0100

    Delete fetching VisitCount API

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,26 +4,11 @@ import React, {useState, useEffect} from "react";
 const Footer = () => {
 
   const year = new Date().getFullYear();
-  const [visitCount, setVisitCount] = useState()
-
-  const updateVisitCount = () => {
-    fetch('https://api.countapi.xyz/update/filip-dev/portfolio/?amount=1')
-        .then(res => res.json())
-        .then(res => {
-            setVisitCount(res.value);
-        })
-  }
-
-  useEffect(()=>{
-    updateVisitCount()
-  },[]);
-
 
   return (
     <>
       <p className="footer paragraph">
         Copyright © {year} - Philippe Lanougadere 
-        {/* - <span id='track'>{visitCount}</span> */}
       </p>
     </>
   );
